7139fdce75a35b64321a0f372ae88504e8d8116f
beenujb <41043473+beenujb@users.noreply.github.com>
2019-01-15 12:00:32 +0530

    Create minimum.py

---
+++ minimum.py
@@ -0,0 +1,11 @@
+# Python program to find smallest  
+# number in a list 
+  
+# list of numbers 
+list1 = [5,4,3,2,1,7,6,8,10,9] 
+  
+# sorting the list 
+list1.sort() 
+  
+# printing the first element 
+print('minimum value:', *list1[:1]) 
