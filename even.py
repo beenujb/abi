267a05c673e4a2e91ed42b8705eeb85fa2c0ff04
beenujb <41043473+beenujb@users.noreply.github.com>
2019-01-15 20:47:42 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,14 @@
+# Python program to check if difference between sum of 
+# odd digits and sum of even digits is 0 or not 
+  
+def isDiff(n): 
+    return (n % 11 == 0) 
+  
+# Driver code 
+n 1= 5;
+if (isDiff(n)): 
+    print("odd") 
+else: 
+    print("even") 
+  
+# Mohit Gupta_OMG <0_o> 
